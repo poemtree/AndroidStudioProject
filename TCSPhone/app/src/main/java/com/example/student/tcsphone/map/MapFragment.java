80be72efc5b460c303d81f5dbf53c1cdbc4d31df
poemtree <yym1232@gmail.com>
2018-05-21 17:18:56 +0900

    map marker add

--- TCSPhone/app/src/main/java/com/example/student/tcsphone/map/MapFragment.java
+++ TCSPhone/app/src/main/java/com/example/student/tcsphone/map/MapFragment.java
@@ -1,9 +1,12 @@
 package com.example.student.tcsphone.map;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +19,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.BufferedReader;
@@ -25,6 +30,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class MapFragment extends Fragment implements FragmentContract.View, OnMapReadyCallback {
@@ -34,11 +41,11 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
 
     private GoogleMap gmap;
     private MapView map;
+    private Marker marker;
+
+    private Bitmap bitmap;
 
     private Button mButton;
-
-    private double lat;
-    private double lng;
 
     private boolean mapFlag;
 
@@ -46,22 +53,6 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
 
     public MapFragment() {
         mapFlag = true;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLat() {
-        return this.lat;
-    }
-
-    public void setLng(double lng) {
-        this.lng = lng;
-    }
-
-    public double getLng() {
-        return this.lng;
     }
 
     @Nullable
@@ -77,6 +68,14 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
         map = (MapView)root.findViewById(R.id.map);
         map.onCreate(mapViewBundle);
         map.getMapAsync(this);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.carmarker);
+
+        //Marker Image resize
+/*
+        bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),
+                R.drawable.carmarker), 30,60,false);
+*/
 
         mButton = (Button)root.findViewById(R.id.btn_map);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -130,16 +129,19 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
         super.onStop();
         map.onStop();
     }
+
     @Override
     public void onPause() {
         map.onPause();
         super.onPause();
     }
+
     @Override
     public void onDestroy() {
         map.onDestroy();
         super.onDestroy();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
@@ -162,15 +164,16 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
         return new MapFragment();
     }
 
-    class LocationTask extends AsyncTask<String, Void, Void> {
+    class LocationTask extends AsyncTask<String, Void, List<Double>> {
 
         @Override
-        protected Void doInBackground(String... strings) {
+        protected List<Double> doInBackground(String... strings) {
 
             String address = "http://70.12.114.134/ws/car.do";
             URL url = null;
             HttpURLConnection con = null;
             BufferedReader br = null;
+            List<Double> list = new ArrayList<Double>();
 
             try {
                 url = new URL(address+"?"+strings[0]+"="+strings[1]);
@@ -187,9 +190,11 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
                     }
                     br = new BufferedReader(new InputStreamReader(con.getInputStream()));
                     StringTokenizer result = new StringTokenizer(br.readLine());
-                    lat = Double.parseDouble(result.nextToken("/"));
+                    /*lat = Double.parseDouble(result.nextToken("/"));
                     lng = Double.parseDouble(result.nextToken("/"));
-
+                    Log.d("What the Fuck!!", String.valueOf(lat) + ", "+ String.valueOf(lng));*/
+                    list.add(Double.parseDouble(result.nextToken("/")));
+                    list.add(Double.parseDouble(result.nextToken("/")));
                 }
 
             } catch (MalformedURLException e) {
@@ -197,35 +202,33 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if(br != null) {
-                    try {
-                        br.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
+                con.disconnect();
             }
-            return null;
+            Log.d("What the Fuck!!", String.valueOf(list.get(0)) + ", "+ String.valueOf(list.get(1)));
+            return list;
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-
-            LatLng CAR = new LatLng(lat, lng);
+        protected void onPostExecute(List<Double> list) {
+            if(marker != null)
+                marker.remove();
+            Log.d("What the Hell!!", "1");
+            LatLng CAR = new LatLng(list.get(0), list.get(1));
 
             MarkerOptions markerOptions = new MarkerOptions();
 
             markerOptions.position(CAR);
 
-            markerOptions.title("차 위치");
+            markerOptions.title("MyCarLocation");
 
-            markerOptions.snippet("내 차");
+            //markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.carmarker));
 
-            gmap.addMarker(markerOptions);
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
+
+            marker = gmap.addMarker(markerOptions);
 
             gmap.moveCamera(CameraUpdateFactory.newLatLng(CAR));
-
+            Log.d("What the Hell!!", "2");
         }
     }
 
@@ -236,7 +239,7 @@ public class MapFragment extends Fragment implements FragmentContract.View, OnMa
             while(mapFlag) {
                 try {
                     LocationTask locationTask = new LocationTask();
-                    locationTask.doInBackground("car_num", "1234567890");
+                    locationTask.execute("car_num", "1234567890");
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
